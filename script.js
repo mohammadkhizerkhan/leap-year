f459441898d40fa2d52aa619a88afd75037d1839
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-08-11 22:29:11 +0530

    gave input type as date and did some css

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ var outputTxt=document.querySelector('#result')
 
 btn.addEventListener('click',function clickHandler(){
     var inputValue=inputTxt.value
+    // console.log(inputValue)
+    var formattedValue=inputValue.split("-")
+    // console.log(formattedValue)
+    // console.log(formattedValue[0])
+
     
-    if ((inputValue % 100 === 0) ? (inputValue % 400 === 0) : (inputValue % 4 === 0)) {
+    if ((formattedValue[0] % 100 === 0) ? (formattedValue[0] % 400 === 0) : (formattedValue[0] % 4 === 0)) {
         // console.log(`${inputValue} is a leap year`);
-        outputTxt.innerHTML=(`${inputValue} is a leap year`)
+        outputTxt.innerHTML=(`${formattedValue[0]} is a leap year`)
     } 
-    else if(isNaN(inputValue)){
+    else if(isNaN(formattedValue[0])){
         outputTxt.innerHTML=("invalid input")
     }
     else {
         // console.log(`${inputValue} is not a leap year`);
-        outputTxt.innerHTML=(`${inputValue} is not a leap year`)
+        outputTxt.innerHTML=(`${formattedValue[0]} is not a leap year`)
     }
 });
